23e8e544a927a380e9f69da26b1be1734b021615
KingKurumi <86593997@qq.com>
2021-12-05 11:51:04 +0800

    Add inductance and mutual inductance mathematical derivations

--- cc/common.py
+++ cc/common.py
@@ -1,80 +1,53 @@
 # common.py
 
-import numpy as np
-from scipy.integrate import quad
-from scipy.special import ellipk, ellipe, j0, j1, struve
-from functools import lru_cache
+from math import pi, sqrt, pow
 
-pi = np.pi
-e  = np.e
-Mu0 = 4 * pi * np.float_power(10, -7)
+from scipy.special import ellipk as eK
+from scipy.special import ellipe as eE
 
-parameterSets = {
-    "r1": 1, "r2": 1, "r3": 1, "r4": 1,
-    "ld": 1, "la": 1,
-    "RhoD": 0, "RhoA": 1,
-    "k": 1, "Sw": 0, "nd": 1,
-    "m": 1, "n": 1,
-}
-
-p = parameterSets
+μ0 = 4 * pi * pow(10, -7)
 
 
-def Rd():
-    '''
-    计算驱动线圈电阻
-    '''
-    V = pi * (p["r2"] * p["r2"] - p["r1"] * p["r1"]) * p["ld"]
-    l = p["k"] * V / p["Sw"]
-    Rd = p["RhoD"] * l / p["SW"]
+class coil():
+    def __init__(self, x0, Ra, Rd):
+        self.Ra = Ra
+        self.Rd = Rd
+        self.x = x0
 
-    return Rd
+    def calcK(self, d):
+        return sqrt((4 * self.Ra * self.Rd) / (pow((self.Ra + self.Rd), 2) + pow(d, 2)))
 
+    def updatePosition(self, delta):
+        self.x += delta
+    
+    def R(self):
+        pass
 
-@lru_cache(maxsize=1)
-def Ra(j):
-    '''
-    计算电枢电流丝电阻
-    '''
-    if j == 1:
-        rj = p["r3"] + j * (p["r4"] - p["r3"]) / p["n"]
-        r = rj - (p["r4"] - p["r3"]) / (2 * p["n"])
-        l = 2 * pi * r
-        Sa = p["la"] * (p["r4"] - p["r3"]) / (p["m"] * p["n"])
-        Ra = p["RhoA"] * l / Sa
+    def L(self):
+        # TODO
+        
+        pass
 
-        return Ra
-    else:
-        return Ra(j - 1) + p["m"] / p["la"]
+    def M(self, others):
+        d = abs(self.x - others.x)
+        k = self.calcK(d)
 
+        return μ0 * sqrt(self.Ra * self.Rd) * ((2 / k - k) * eK(k) - (2 / k) * eE(k))
 
-def U(p,x):
-    pass
+    def dM(self, others):
+        d = abs(self.x - others.x)
+        k = self.calcK(d)
 
-def T_integration():
-    pass
+        return (μ0 * k * d * (2 * (1 - pow(k, 2)) * eK(k) - (2 - pow(k, 2)) * eE(k))) / (4 * (1 - pow(k, 2)) * sqrt(self.Ra * self.Rd))
 
-def T(p, q):
-    return quad(T_integration, 0, float("inf"))
+class drivingCoil(coil):
+    def R(self):
+        # TODO
+        
+        pass
 
-
-def L(nc, rci, rco, lc):
-    return 2 * pi * Mu0 * np.power(nc, 2) * np.power(rci, 5) * T(rco / rci, lc / rci)
-
-def Ld():
-    return L(p["nc"], p["r1"], p["r2"], p["ld"])
-
-
-da = (p["r4"] - p["r3"]) / p["n"]
-lc = p["la"] / p["m"]
-
-
-def La(j):
-    rci = p["r3"] + j * da
-
-    return L(1, rci, rci + da, lc)
-
-
-# DEBUG
-print("[OK]INITED")
-print(T(1.4, 1.86))
+class armature(coil):
+    def R(self):
+        # TODO
+        
+        pass
