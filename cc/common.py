78b3de6200fb0767f2c3d261d9fe5a2d52d326de
KingKurumi <86593997@qq.com>
2022-03-14 10:53:00 +0800

    V0.0.4

--- cc/common.py
+++ cc/common.py
@@ -1,53 +1,80 @@
 # common.py
 
-from math import pi, sqrt, pow
+from math import e, pi, pow, sqrt
 
-from scipy.special import ellipk as eK
+import numpy as np
+
+from scipy.integrate import quad
+from scipy.special import j0, j1, struve
 from scipy.special import ellipe as eE
+from scipy.special import ellipk as eK
+
 
 μ0 = 4 * pi * pow(10, -7)
 
 
 class coil():
-    def __init__(self, x0, Ra, Rd):
-        self.Ra = Ra
-        self.Rd = Rd
+    def __init__(self, x0, nc, rci, rco, l):
         self.x = x0
+        self.nc = nc    #BUG
+        self.rci = rci
+        self.rco = rco
+        self.l = l
+        
+        self.Ra = (rci + rco) / 2
+        self.p = self.rco / self.rci
+        self.q = self.l / self.rci
+        
+        # U = lambda x: quad(lambda x: x * j1(x), x, p * x)[0] / pow(x, 3)
+        U = lambda x: pi * (-j1(x) * struve(0, x)  +  self.p * j1(self.p * x) * struve(0, self.p * x)  + j0(x) * struve(1, x)  -  self.p * j0(self.p * x) * struve(1, self.p * x)) / (2 * pow(x,2))
+        self.integrationT = lambda x: pow(U(x), 2) * (self.q * x  +  pow(e,(-1 * self.q * x))  -  1)
+        
+        self.R = self.R()
+        self.L = self.L()
 
-    def calcK(self, d):
-        return sqrt((4 * self.Ra * self.Rd) / (pow((self.Ra + self.Rd), 2) + pow(d, 2)))
+    def T(self):
+        return quad(self.integrationT, 0, np.inf, limit=100)
+
+    def calcK(self, others, d):
+        return sqrt((4 * self.Ra * others.Ra) / (pow((self.Ra + others.Ra), 2) + pow(d, 2)))
 
     def updatePosition(self, delta):
         self.x += delta
-    
+
     def R(self):
         pass
 
     def L(self):
-        # TODO
-        
-        pass
+        return 2 * pi * μ0 * np.power(self.nc, 2) * np.power(self.rci, 5) * self.T()
 
     def M(self, others):
         d = abs(self.x - others.x)
-        k = self.calcK(d)
+        k = self.calcK(others, d)
 
-        return μ0 * sqrt(self.Ra * self.Rd) * ((2 / k - k) * eK(k) - (2 / k) * eE(k))
+        return μ0 * sqrt(self.Ra * others.Ra) * ((2 / k - k) * eK(k) - (2 / k) * eE(k))
 
     def dM(self, others):
         d = abs(self.x - others.x)
-        k = self.calcK(d)
+        k = self.calcK(others, d)
 
-        return (μ0 * k * d * (2 * (1 - pow(k, 2)) * eK(k) - (2 - pow(k, 2)) * eE(k))) / (4 * (1 - pow(k, 2)) * sqrt(self.Ra * self.Rd))
+        return (μ0 * k * d * (2 * (1 - pow(k, 2)) * eK(k) - (2 - pow(k, 2)) * eE(k))) / (4 * (1 - pow(k, 2)) * sqrt(self.Ra * others.Ra))
+
 
 class drivingCoil(coil):
+    def __init__(self, x0, nc, rci, rco, l):
+        coil.__init__(self, x0, nc, rci, rco, l)
+    
     def R(self):
         # TODO
-        
+
         pass
 
+
 class armature(coil):
+    def __init__(self, x0, nc, rci, rco, l):
+        coil.__init__(self, x0, nc, rci, rco, l)
+    
     def R(self):
         # TODO
-        
+
         pass
