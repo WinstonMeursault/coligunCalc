898c4535b7407b37dfa9f8d1467992558e7f9ee9
KingKurumi <86593997@qq.com>
2022-03-25 18:32:32 +0800

    V0.0.9

--- basics.py
+++ basics.py
@@ -13,29 +13,34 @@ from scipy.special import ellipk as eK
 μ0 = 4 * pi * pow(10, -7)
 
 
-def L(coilA, limit = 125):
+def L(coilA, limit=125):
     p = coilA.re / coilA.ri
     q = coilA.l / coilA.ri
-        
+
     # U = lambda x: quad(lambda x: x * j1(x), x, p * x)[0] / pow(x, 3)
-    U = lambda x: pi * (-j1(x) * struve(0, x)  +  p * j1(p * x) * struve(0, p * x)  + j0(x) * struve(1, x)  -  p * j0(p * x) * struve(1, p * x)) / (2 * pow(x,2))
-        
-    integrationT = lambda x: pow(U(x), 2) * (q * x  +  pow(e,(-1 * q * x))  -  1)
-    T = quad(integrationT, 0, np.inf, limit = limit)[0]
-        
+    def U(x): return pi * (-j1(x) * struve(0, x) + p * j1(p * x) * struve(0, p * x) +
+                           j0(x) * struve(1, x) - p * j0(p * x) * struve(1, p * x)) / (2 * pow(x, 2))
+
+    def integrationT(x): return pow(U(x), 2) * \
+        (q * x + pow(e, (-1 * q * x)) - 1)
+    T = quad(integrationT, 0, np.inf, limit=limit)[0]
+
     return 2 * pi * μ0 * np.power(coilA.nc, 2) * np.power(coilA.ri, 5) * T
+
 
 def calcK(coilA, coilB, d):
     return sqrt((4 * coilA.r * coilB.r) / (pow((coilA.r + coilB.r), 2) + pow(d, 2)))
 
-def M(coilA, coilB, d = None):
+
+def M(coilA, coilB, d=None):
     if d == None:
         d = abs(coilA.x - coilB.x)
     k = calcK(coilA, coilB, d)
 
     return μ0 * sqrt(coilA.r * coilB.r) * ((2 / k - k) * eK(k) - (2 / k) * eE(k))
 
-def dM(coilA, coilB, d = None):
+
+def dM(coilA, coilB, d=None):
     if d == None:
         d = abs(coilA.x - coilB.x)
     k = calcK(coilA, coilB, d)
@@ -44,7 +49,7 @@ def dM(coilA, coilB, d = None):
 
 
 class drivingCoil():
-    def __init__(self,rdi, rde, ld, n, x0, resistivity, s, k):
+    def __init__(self, rdi, rde, ld, n, x0, resistivity, s, k):
         self.ri = rdi
         self.re = rde
         self.l = ld
@@ -53,20 +58,21 @@ class drivingCoil():
         self.SR = resistivity           # 电阻率
         self.s = s                      # 单根导线的截面积
         self.k = k                      # 驱动线圈填充率
-        
+
         self.r = (self.ri + self.re) / 2
         Sd = (self.re - self.ri) * self.l
-        
+
         self.nc = self.n / Sd
-        
+
         self.R = self.R()
         self.L = L(self)
 
     def R(self):
         return (self.SR * self.k * pi * (pow(self.re, 2) - pow(self.ri, 2)) * self.l) / pow(self.s, 2)
 
+
 class armature():
-    def __init__(self,rai, rae, la, x0, resistivity, m, n):
+    def __init__(self, rai, rae, la, x0, resistivity, m, n):
         self.ri = rai
         self.re = rae
         self.l = la
@@ -74,21 +80,31 @@ class armature():
         self.SR = resistivity
         self.m = m
         self.n = n
-        
+
         self.currentFilaments = {}
         for i in range(1, self.m + 1):
             for j in range(1, self.n + 1):
                 self.currentFilaments[i][j] = {
                     "r": None,
-                    "R":None,
-                    "L":None
+                    "R": None,
+                    "L": None
                 }
-    
-    def R(self):
-        deltaR = 2 * pi * self.SR * self.m / self.l
-        for k in self.currentFilaments:
-            for l in range(1, self.m + 1):
-                pass    # TODO
-    
+
     def updatePosition(self, delta):
         self.x += delta
+
+    def R(self):
+        deltaR = 2 * pi * self.SR * self.m / self.l
+        R = {
+            1: 2 * pi * self.SR * ((self.m / (2 * self.l)) + (self.m * self.n * self.ri / (self.l * (self.re - self.ri))))
+        }
+
+        for a in range(2, self.n + 1):
+            R[a] = R[a - 1] + deltaR
+
+        for k in self.currentFilaments:
+            for l in range(1, self.n + 1):
+                k[l]["R"] = R[l]
+    
+    def L(self):
+        pass                        # TODO
