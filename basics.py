2c9b6cb89cc409c842f2038cbd555ce7e07e8c52
Winston Meursault <86593997@qq.com>
2022-07-06 13:59:25 +0800

    update

--- basics.py
+++ basics.py
@@ -1,15 +1,12 @@
 # common.py
 
-
-import numpy as np
-
-from scipy.integrate import quad
-from scipy.special import j0, j1, struve
-from scipy.special import ellipe as eE
-from scipy.special import ellipk as eK
-
 from functools import lru_cache
 
+import numpy as np
+from scipy.integrate import quad
+from scipy.special import ellipe as eE
+from scipy.special import ellipk as eK
+from scipy.special import j0, j1, struve
 
 μ0 = 4 * np.pi * np.power(10, -7)
 
@@ -59,7 +56,80 @@ class currentFilament():
         self.r = (self.ri + self.re) / 2
 
         Sc = l * (self.re - self.ri)
-        self.nc = 1 / Sc    
+        self.nc = 1 / Sc
 
     def updatePosition(self, delta):
         self.x += delta
+
+
+class drivingCoil():
+    def __init__(self, rdi, rde, ld, n, resistivity, Swire, k):
+        self.ri = rdi
+        self.re = rde
+        self.l = ld
+        self.n = n                      # 线圈匝数
+        self.x = 0.5 * self.l
+        self.SR = resistivity           # 电阻率
+        self.Swire = Swire              # 单根导线的截面积
+        self.k = k                      # 驱动线圈填充率
+
+        self.r = (self.ri + self.re) / 2
+        Sd = (self.re - self.ri) * self.l
+
+        self.nc = self.n / Sd
+
+        self.R = self.R()
+        self.L = L(self)
+
+    def R(self):
+        return (self.SR * self.k * np.pi * (np.power(self.re, 2) - np.power(self.ri, 2)) * self.l) / np.power(self.Swire, 2)
+
+
+class armature():
+    def __init__(self, rai, rae, la, resistivity, v0, ma, m, n, x0):
+        self.ri = rai
+        self.re = rae
+        self.l = la
+        self.SR = resistivity
+        self.v0 = v0
+        self.ma = ma
+        self.m = m
+        self.n = n
+
+        self.x = x0
+
+        self.currentFilaments = {}
+
+        for i in range(1, self.m + 1):
+            for j in range(1, self.n + 1):
+                self.currentFilaments[i][j] = currentFilament(ri=self.currentFilamentR(j - 1), re=self.currentFilamentR(j),
+                                                              l=self.l / self.m, R=None, L=None, x0=self.l * (i - 0.5) / self.m)
+
+        self.R()
+        self.L()
+
+    def currentFilamentR(self, j):
+        return self.ri + (self.re - self.ri) * j / self.n
+
+    def updatePosition(self, delta):
+        for i in range(1, self.m + 1):
+            for j in range(1, self.n + 1):
+                self.currentFilaments[i][j].updatePosition(delta)
+
+    def R(self):
+        deltaR = 2 * np.pi * self.SR * self.m / self.l
+        R = {
+            1: 2 * np.pi * self.SR * ((self.m / (2 * self.l)) + (self.m * self.n * self.ri / (self.l * (self.re - self.ri))))
+        }
+
+        for a in range(2, self.n + 1):
+            R[a] = R[a - 1] + deltaR
+
+        for i in range(1, self.m + 1):
+            for j in range(1, self.n + 1):
+                self.currentFilaments[i][j].R = R[j]
+
+    def L(self):
+        for i in range(1, self.m + 1):
+            for j in range(1, self.n + 1):
+                self.currentFilaments[i][j].L = L(self.currentFilaments[i][j])
--- core.py
+++ core.py
@@ -1,82 +1,9 @@
 # singleStage.py
 
-import numpy.matlib 
 import numpy as np
+import numpy.matlib
 
 from basics import *
-
-
-class drivingCoil():
-    def __init__(self, rdi, rde, ld, n, resistivity, Swire, k):
-        self.ri = rdi
-        self.re = rde
-        self.l = ld
-        self.n = n                      # 线圈匝数
-        self.x = 0.5 * self.l
-        self.SR = resistivity           # 电阻率
-        self.Swire = Swire              # 单根导线的截面积
-        self.k = k                      # 驱动线圈填充率
-
-        self.r = (self.ri + self.re) / 2
-        Sd = (self.re - self.ri) * self.l
-
-        self.nc = self.n / Sd
-
-        self.R = self.R()
-        self.L = L(self)
-
-    def R(self):
-        return (self.SR * self.k * np.pi * (np.power(self.re, 2) - np.power(self.ri, 2)) * self.l) / np.power(self.Swire, 2)
-
-
-class armature():
-    def __init__(self, rai, rae, la, resistivity, v0, ma, m, n, x0):
-        self.ri = rai
-        self.re = rae
-        self.l = la
-        self.SR = resistivity
-        self.v0 = v0
-        self.ma = ma
-        self.m = m
-        self.n = n
-
-        self.x = x0
-
-        self.currentFilaments = {}
-
-        for i in range(1, self.m + 1):
-            for j in range(1, self.n + 1):
-                self.currentFilaments[i][j] = currentFilament(ri=self.currentFilamentR(j - 1), re=self.currentFilamentR(j),
-                                                              l=self.l / self.m, R=None, L=None, x0=self.l * (i - 0.5) / self.m)
-
-        self.R()
-        self.L()
-
-    def currentFilamentR(self, j):
-        return self.ri + (self.re - self.ri) * j / self.n
-
-    def updatePosition(self, delta):
-        for i in range(1, self.m + 1):
-            for j in range(1, self.n + 1):
-                self.currentFilaments[i][j].updatePosition(delta)
-
-    def R(self):
-        deltaR = 2 * np.pi * self.SR * self.m / self.l
-        R = {
-            1: 2 * np.pi * self.SR * ((self.m / (2 * self.l)) + (self.m * self.n * self.ri / (self.l * (self.re - self.ri))))
-        }
-
-        for a in range(2, self.n + 1):
-            R[a] = R[a - 1] + deltaR
-
-        for i in range(1, self.m + 1):
-            for j in range(1, self.n + 1):
-                self.currentFilaments[i][j].R = R[j]
-
-    def L(self):
-        for i in range(1, self.m + 1):
-            for j in range(1, self.n + 1):
-                self.currentFilaments[i][j].L = L(self.currentFilaments[i][j])
 
 
 class singleStageCoilgun():
@@ -109,29 +36,33 @@ class singleStageCoilgun():
 
         # 计算常数矩阵[M]       各电阻丝之间的互感
         # TODO
-        self.M = np.zeros((self.armature.m * self.armature.n + 1, self.armature.m * self.armature.n + 1))
+        self.M = np.zeros((self.armature.m * self.armature.n + 1,self.armature.m * self.armature.n + 1))
 
         # 初始化时变矩阵[M1]    驱动线圈与各电阻丝之间的互感
         self.M1 = np.zeros((self.armature.m * self.armature.n + 1, self.armature.m * self.armature.n + 1))
         for i in range(1, self.armature.m + 1):
             for j in range(1, self.armature.n + 1):
-                self.M1[0][(i - 1) * self.armature.n + j] = M(self.drivingCoil.r, self.armature.currentFilaments[i][j].r, 
-                                                                abs(self.armature.currentFilaments[i][j].r - self.drivingCoil.x))
+                self.M1[0][(i - 1) * self.armature.n + j] = M(self.drivingCoil.r, self.armature.currentFilaments[i][j].r,
+                                                              abs(self.armature.currentFilaments[i][j].r - self.drivingCoil.x))
+
+        self.M1 = self.M1 + self.M1.T
 
         # 初始化时变矩阵[dM1/dx]    驱动线圈与各电阻丝之间的互感梯度
         self.dM = np.zeros((self.armature.m * self.armature.n + 1, self.armature.m * self.armature.n + 1))
         for i in range(1, self.armature.m + 1):
             for j in range(1, self.armature.n + 1):
-                self.M1[0][(i - 1) * self.armature.n + j] = dM(self.drivingCoil.r, self.armature.currentFilaments[i][j].r, 
-                                                                abs(self.armature.currentFilaments[i][j].r - self.drivingCoil.x))
+                self.M1[0][(i - 1) * self.armature.n + j] = dM(self.drivingCoil.r, self.armature.currentFilaments[i][j].r,
+                                                               abs(self.armature.currentFilaments[i][j].r - self.drivingCoil.x))
+
+        self.dM = self.dM + self.dM.T
 
         # 初始化时变矩阵[U]     电容电压
-        self.U = np.matrix([U] + [0] * self.armature.m * self.armature.n).T 
+        self.U = np.matrix([U] + [0] * self.armature.m * self.armature.n).T
 
         # 初始化待求矩阵[I] / [dI]     驱动回路电流及其导
-        self.I  = np.matlib.zeros((self.armature.m * self.armature.n + 1, 1))
+        self.I = np.matlib.zeros((self.armature.m * self.armature.n + 1, 1))
         self.Id = self.U / (self.L - self.M1)
-    
+
     def __delattr__(self, __name):
         M.cache_clear()
         dM.cache_clear()
@@ -155,7 +86,7 @@ class singleStageCoilgun():
     def __runT(self):
         pass
 
-    def run(self,x):    
+    def run(self, x):
         pass
 
 
