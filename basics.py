9fd648600f3ffed7942055fb2b76fc390ad9a59b
Winston Meursault <86593997@qq.com>
2022-08-23 08:38:36 +0800

    Stabilize pure Python codebase: fix resistance and inductance bugs, add comprehensive API docstrings

--- basics.py
+++ basics.py
@@ -1,7 +1,8 @@
 from functools import lru_cache
+from typing import List
 
 import numpy as np
-from scipy.integrate import quad
+from scipy.integrate import quad, nquad
 from scipy.special import ellipe as eE
 from scipy.special import ellipk as eK
 from scipy.special import j0, j1, struve
@@ -9,15 +10,28 @@ from scipy.special import j0, j1, struve
 μ0 = 0.0000012566370614359173
 
 
-def calcL(ri, re, l, nc, limit=200):
+def calcL(ri: float, re: float, l: float, nc: int, limit: int = 200) -> float:
+    """计算线圈自感
+
+    Args:
+        ri (float): 线圈内径
+        re (float): 线圈外径
+        l (float): 线圈长度
+        nc (int): 线圈匝数
+        limit (int, optional): Numpy quad参数, 若警告区间划分数过少可适当提高. Defaults to 200.
+
+    Returns:
+        float: 线圈自感
+    """    
+    
     p = re / ri
     q = l / ri
 
     # U = lambda x: quad(lambda x: x * j1(x), x, p * x)[0] / np.power(x, 3)
-    def U(x):
-        return np.pi * (-j1(x) * struve(0, x) + p * j1(p * x) * struve(0, p * x) + j0(x) * struve(1, x) - p * j0(p * x) * struve(1, p * x)) / (2 * np.power(x, 2))
+    def U(x: float) -> float:
+        return np.pi * (-j1(x) * struve(0, x) + p * j1(p * x) * struve(0, p * x) + j0(x) * struve(1, x) - p * j0(p * x) * struve(1, p * x)) / (2 * np.power(x, 2))  
 
-    def integrationT(x):
+    def integrationT(x: float) -> float:
         return np.power(U(x), 2) * (q * x + np.power(np.e, (-1 * q * x)) - 1)
 
     T = quad(integrationT, 0, np.inf, limit=limit)[0]
@@ -25,26 +39,72 @@ def calcL(ri, re, l, nc, limit=200):
     return 2 * np.pi * μ0 * np.power(nc, 2) * np.power(ri, 5) * T
 
 
-def calcK(Ra, Rb, d):
+def calcK(Ra: float, Rb: float, d: float) -> float:
     return np.sqrt((4 * Ra * Rb) / (np.power(Ra + Rb, 2) + np.power(d, 2)))
 
 
 @lru_cache()
-def calcM(Ra, Rb, d):
-    k = calcK(Ra, Rb, d)
+def calcM(Rai: float, Rae: float, La: float, Na: int, Rbi: float, Rbe: float, Lb: float, Nb: int, d: float) -> float:
+    """计算两线圈互感(设为A B线圈)
 
-    return μ0 * np.sqrt(Ra * Rb) * ((2 / k - k) * eK(k) - (2 / k) * eE(k))
+    Args:
+        Rai (float): A线圈内径
+        Rae (float): A线圈外径
+        La (float): A线圈长度
+        Na (int): A线圈匝数
+        Rbi (float): B线圈内径
+        Rbe (float): B线圈外径
+        Lb (float): B线圈长度
+        Nb (int): B线圈匝数
+        d (float): A B线圈中心面的距离
+
+    Returns:
+        float: A B两线圈之间的互感(返回在距离为d时的数值)
+    """      
+
+    def integrationM(r1: float, z1: float, r2: float, z2: float) -> float:
+        ra = Rai + r1
+        rb = Rbi + r2
+        dz = abs(abs(d - 0.5 * La + 0.5 * Lb) + z1 - z2)
+
+        k = calcK(ra, rb, dz)
+        return μ0 * np.sqrt(ra * rb) * ((2 / k - k) * eK(k) - (2 / k) * eE(k))
+
+    return (Na * Nb * nquad(integrationM, [[0, Lb], [Rbi, Rbe], [0, La], [Rai, Rbe]])[0]) / ((Rae - Rai) * La * (Rbe - Rbi) * Lb)
 
 
 @lru_cache()
-def calcdM(Ra, Rb, d):
-    k = calcK(Ra, Rb, d)
+def calcdM(Rai: float, Rae: float, La: float, Na: int, Rbi: float, Rbe: float, Lb: float, Nb: int, d: float) -> float:  
+    """计算两线圈互感梯度(设为A B线圈)
 
-    return (μ0 * k * d * (2 * (1 - np.power(k, 2)) * eK(k) - (2 - np.power(k, 2)) * eE(k))) / (4 * (1 - np.power(k, 2)) * np.sqrt(Ra * Rb))
+    Args:
+        Rai (float): A线圈内径
+        Rae (float): A线圈外径
+        La (float): A线圈长度
+        Na (int): A线圈匝数
+        Rbi (float): B线圈内径
+        Rbe (float): B线圈外径
+        Lb (float): B线圈长度
+        Nb (int): B线圈匝数
+        d (float): A B线圈中心面的距离
+
+    Returns:
+        float: A B两线圈之间的互感梯度(返回在距离为d时的数值)
+    """      
+
+    def integrationdM(r1: float, z1: float, r2: float, z2: float) -> float:
+        ra = Rai + r1
+        rb = Rbi + r2
+        dz = abs(abs(d - 0.5 * La + 0.5 * Lb) + z1 - z2)
+
+        k = calcK(ra, rb, dz)
+        return (μ0 * k * d * (2 * (1 - np.power(k, 2)) * eK(k) - (2 - np.power(k, 2)) * eE(k))) / (4 * (1 - np.power(k, 2)) * np.sqrt(ra * rb))
+
+    return (Na * Nb * nquad(integrationdM, [[0, Lb], [Rbi, Rbe], [0, La], [Rai, Rbe]])[0]) / ((Rae - Rai) * La * (Rbe - Rbi) * Lb)
 
 
 class drivingCoil():
-    def __init__(self, rdi, rde, ld, n, resistivity, Swire, k):
+    def __init__(self, rdi: float, rde: float, ld: float, n: int, resistivity: float, Swire: float, k: float) -> None:
         self.ri = rdi
         self.re = rde
         self.l = ld
@@ -58,13 +118,12 @@ class drivingCoil():
 
         self.nc = self.n / ((self.re - self.ri) * self.l)
 
-        self.R = (self.SR * self.k * np.pi * (np.power(self.re, 2) -
-                  np.power(self.ri, 2)) * self.l) / np.power(self.Swire, 2)
+        self.R = (self.SR * self.k * np.pi * (np.power(self.re, 2) - np.power(self.ri, 2)) * self.l) / np.power(self.Swire, 2)
         self.L = calcL(self.ri, self.re, self.l, self.nc)
 
 
 class armature():
-    def __init__(self, rai, rae, la, resistivity, v0, ma, m, n, x0, limit=200):
+    def __init__(self, rai: float, rae: float, la: float, resistivity: float, v0: float, ma: float, m: int, n: int, x0: float, limit: int = 200) -> None:
         self.ri = rai
         self.re = rae
         self.l = la
@@ -76,43 +135,77 @@ class armature():
         self.x = x0
 
         self.__deltaRN = (self.re - self.ri) / self.n
-        self.__currentFilamentL = self.l / self.m
-        self.__currentFilamentNc = 1 / \
-            (self.__currentFilamentL * self.__deltaRN)
+        self.currentFilamentL = self.l / self.m
+        self.__currentFilamentNc = 1 / (self.currentFilamentL * self.__deltaRN)
 
         self.R = self.__R()
         self.L = self.__L(limit)
 
-    def __currentFilamentRi(self, j):
+    def currentFilamentRi(self, j: int) -> float: 
+        """计算电流丝内径
+
+        Args:
+            j (int): 电流丝径向编号
+
+        Returns:
+            float: 电流丝内径
+        """             
         return self.ri + (self.re - self.ri) * (j - 1) / self.n
 
-    def __currentFilamentRe(self, j):
+    def currentFilamentRe(self, j: int) -> float:
+        """计算电流丝外径
+
+        Args:
+            j (int): 电流丝径向编号
+
+        Returns:
+            float: 电流丝外径
+        """        
         return self.ri + (self.re - self.ri) * j / self.n
 
-    def currentFilamentAR(self, j):
+    def currentFilamentAR(self, j: int) -> float:
+        """计算电流丝的平均半径
+
+        Args:
+            j (int): 电流丝径向编号
+
+        Returns:
+            float: 电流丝平均半径
+        """        
         return self.ri + self.__deltaRN * (j - 0.5)
 
-    def currentFilamentX(self, i):
-        return self.x - 0.5 * self.l + (i - 0.5) * self.__currentFilamentL
+    def currentFilamentX(self, i: int) -> float:
+        """计算电流丝绝对位置
 
-    def updatePosition(self, delta):
+        Args:
+            i (int): 电流丝轴向编号
+
+        Returns:
+            float: 电流丝的绝对位置
+        """        
+        return self.x - 0.5 * self.l + (i - 0.5) * self.currentFilamentL
+
+    def updatePosition(self, delta: float) -> None:
+        """更新电枢位置(将位置更新为原位置与delta之和)
+
+        Args:
+            delta (float): 电枢位移
+        """        
         self.x += delta
 
-    def __R(self):
+    def __R(self) -> List[float]:
         deltaR = 2 * np.pi * self.SR * self.m / self.l
-        R = [2 * np.pi * self.SR * ((self.m / (2 * self.l)) + (
-            self.m * self.n * self.ri / (self.l * (self.re - self.ri))))]
+        R = [2 * np.pi * self.SR * ((self.m / (2 * self.l)) + (self.m * self.n * self.ri / (self.l * (self.re - self.ri))))]
 
-        for k in range(0, self.n):
-            R.append(R[k] + deltaR)
+        for k in range(1, self.n):
+            R.append(R[k - 1] + deltaR)
 
         return R * self.m
 
-    def __L(self, limit):
+    def __L(self, limit: int) -> List[float]:
         L = []
 
         for l in range(1, self.n + 1):
-            L.append(calcL(self.__currentFilamentRi(l), self.__currentFilamentRe(l),
-                     self.__currentFilamentL, self.__currentFilamentNc, limit))
+            L.append(calcL(self.currentFilamentRi(l), self.currentFilamentRe(l), self.currentFilamentL, self.__currentFilamentNc, limit))
 
         return L * self.m
--- core.py
+++ core.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import numpy as np
 import numpy.matlib
 
@@ -8,33 +10,40 @@ class singleStageCoilgun():
     def __init__(self, drivingCoil, armature, U, C, deltaT):
         self.drivingCoil = drivingCoil
         self.armature = armature
+        self.Uc = U
         self.C = C
         self.dt = deltaT
-        
-        self.t = 0
-        
-        self.U  = np.matrix([U] + [0] * self.armature.m * self.armature.n).T
-        self.I  = np.mat(np.zeros((self.armature.m * self.armature.n + 1, 1)))
-        self.Id = np.mat(np.zeros((self.armature.m * self.armature.n + 1, 1)))
 
-        self.cache = {"U(n-1)" : None, "I(n-1)" : None, "Id(n-1)" : None}
+        self.t = 0
+
+        self.U = np.matrix([self.Uc] + [0] * self.armature.m * self.armature.n).T
+        self.I = np.matrix(np.zeros((self.armature.m * self.armature.n + 1, 1)))
+        self.Id = np.matrix(np.zeros((self.armature.m * self.armature.n + 1, 1)))
+
         self.__cache()
 
-        self.R = np.mat(np.diag([self.drivingCoil.R] + self.armature.R))
+        self.R = np.matrix(np.diag([self.drivingCoil.R] + self.armature.R))
 
-        self.L = np.mat(np.diag([self.drivingCoil.L] + self.armature.L))
+        self.L = np.matrix(np.diag([self.drivingCoil.L] + self.armature.L))
 
-        self.M = np.mat(np.zeros((self.armature.m * self.armature.n + 1, self.armature.m * self.armature.n + 1)))
+        self.M = np.matrix(np.zeros((self.armature.m * self.armature.n + 1, self.armature.m * self.armature.n + 1)))
         for i in range(1, self.armature.m + 1):
             for j in range(1, self.armature.n + 1):
                 for k in range(1, self.armature.m + 1):
                     for l in range(1, self.armature.n + 1):
-                        self.M[(i - 1) * self.armature.n + j][(k - 1) * self.armature.n + l] = calcM(self.drivingCoil.r, self.armature.currentFilamentAR(l),
-                                                                                                     abs(self.armature.currentFilamentX(k) - self.drivingCoil.x))
-        for x in range(0, self.M.shape[0]):
-            for y in range(0, self.M.shape[1]):
+                        self.M[(i - 1) * self.armature.n + j, (k - 1) * self.armature.n + l] = \
+                            calcM(self.armature.currentFilamentRi(j), self.armature.currentFilamentRe(j),self.armature.currentFilamentL, 1, 
+                                self.armature.currentFilamentRi(l), self.armature.currentFilamentRe(l),self.armature.currentFilamentL, 1, 
+                                    abs(self.armature.currentFilamentX(i) - self.armature.currentFilamentX(k)))
+        for x in range(1, self.M.shape[0]):
+            for y in range(1, self.M.shape[1]):
                 if x == y:
-                    self.M[x][y] = 0
+                    self.M[x, y] = 0
+
+        self.__updateM1()
+        self.__updatedM1()
+
+        self.Id = np.linalg.inv(self.L - self.M1) * self.U
 
         self.F = 0
         self.a = 0
@@ -45,50 +54,59 @@ class singleStageCoilgun():
         calcdM.cache_clear()
 
         super().__delattr__(__name)
-
+    
     def __updateM1(self):
-        self.M1 = np.mat(np.zeros((self.armature.m * self.armature.n + 1, self.armature.m * self.armature.n + 1)))
+        self.M1 = np.matrix(np.zeros((self.armature.m * self.armature.n + 1, self.armature.m * self.armature.n + 1)))
         for i in range(1, self.armature.m + 1):
             for j in range(1, self.armature.n + 1):
-                self.M1[0][(i - 1) * self.armature.n + j] = calcM(self.drivingCoil.r, self.armature.currentFilamentAR(j),
-                                                                  abs(self.armature.currentFilamentX(i) - self.drivingCoil.x))
+                self.M1[0, (i - 1) * self.armature.n + j] = \
+                    calcM(self.armature.currentFilamentRi(j), self.armature.currentFilamentRe(j),self.armature.currentFilamentL, 1, 
+                        self.drivingCoil.ri, self.drivingCoil.re, self.drivingCoil.l, self.drivingCoil.n, 
+                            abs(self.armature.currentFilamentX(i) - self.drivingCoil.x))
         self.M1 = self.M1 + self.M1.T
 
     def __updatedM1(self):
-        self.dM1 = np.mat(np.zeros((self.armature.m * self.armature.n + 1, self.armature.m * self.armature.n + 1)))
+        self.dM1 = np.matrix(np.zeros((self.armature.m * self.armature.n + 1, self.armature.m * self.armature.n + 1)))
         for i in range(1, self.armature.m + 1):
             for j in range(1, self.armature.n + 1):
-                self.dM1[0][(i - 1) * self.armature.n + j] = calcdM(self.drivingCoil.r, self.armature.currentFilamentAR(j),
-                                                                   abs(self.armature.currentFilamentX(i) - self.drivingCoil.x))
+                self.dM1[0, (i - 1) * self.armature.n + j] = \
+                    calcdM(self.armature.currentFilamentRi(j), self.armature.currentFilamentRe(j),self.armature.currentFilamentL, 1, 
+                        self.drivingCoil.ri, self.drivingCoil.re, self.drivingCoil.l, self.drivingCoil.n, 
+                            abs(self.armature.currentFilamentX(i) - self.drivingCoil.x))
         self.dM1 = self.dM1 + self.dM1.T
 
     def __cache(self):
-        self.cache["U(n-1)"] = self.U
-        self.cache["I(n-1)"] = self.I
-        self.cache["Id(n-1)"] = self.Id
+        self.cache = None               # 防止self.cache缓存上一次的self.chache
+        self.cache = deepcopy(self)
 
     def __update(self):
+        self.U = np.matrix([self.cache.U[0, 0] - self.dt * self.cache.Id[0, 0]] + [0] * self.armature.m * self.armature.n).T
+
+        self.Id = np.linalg.inv(self.L - self.M1) * (self.U + self.Va * self.dM1 * self.I - self.R * self.I - self.M * self.I)
+        self.I = self.cache.I + self.dt * self.cache.Id
+
+        self.F = 0
+        for i in range(1, self.armature.m + 1):
+            for j in range(1, self.armature.n + 1):
+                self.F += self.dM1[0, self.armature.n * (i - 1) + j] * self.I[0, 0] * self.I[self.armature.n * (i - 1) + j, 0]
+        self.F = -1 * self.F
+
+        self.a = self.F / self.armature.ma
+        self.Va = self.cache.Va + self.cache.a * self.dt
+        self.armature.x += self.cache.Va * self.dt
+
         self.__updateM1()
         self.__updatedM1()
-        
-        self.U = np.matrix([self.cache["U(n-1)"][0][0] - self.dt * self.cache["Id(n-1)"][0][0]] + [0] * self.armature.m * self.armature.n).T
-        
-        self.Id = (self.U + self.Va * self.dM1 * self.I - self.R * self.I - self.M * self.I) / (self.L - self.M1)       # BUG
-        self.I  = self.cache["I(n-1)"] + self.dt * self.cache["Id(n-1)"]
-        
-        self.F = 0
-        for i in range(1, self.armature.m * self.armature.n + 1):
-            self.F += self.dM1[0][i] * self.I[0][i]
-        self.F = -1 * self.I[0][0] * self.F
 
-        
+        print("[DEBUG] F = " + str(self.F) + "N, Va = " + str(self.Va) + "m/s, x = " + str(self.armature.x) + "m.")
+
     def run(self, xn):
         while self.armature.x < xn:
             self.__cache()
             self.__update()
-        
-        η = (self.armature.ma * (np.power(self.Va, 2) - np.power(self.armature.v0, 2))) / (self.C * np.power(self.U, 2))
-        
+
+        η = (self.armature.ma * (np.power(self.Va, 2) - np.power(self.armature.v0, 2))) / (self.C * np.power(self.Uc, 2))
+
         return (η, self.Va)
 
 
@@ -98,23 +116,25 @@ class multiStageCoilgun():
 
 # ----------------------------------------------------------------TESTS ----------------------------------------------------------------
 
+
 def TestA():
     import time
     tic = time.perf_counter()
 
-    print("[INFO]READY...")
+    print("[TEST]TEST A: READY----------------------------------------------------------------")
 
-    dc = drivingCoil(0.015125, 0.029125, 0.063, 63, 0.0000000175, 0.000028, 0.7)
+    dc = drivingCoil(0.015125, 0.029125, 0.063, 63,
+                     0.0000000175, 0.000028, 0.7)
     print("[OK]dc initialized")
 
-    # a = armature(0.012, 0.015, 0.07, 0.0000000175, 0, 6.384, 70000, 3000, 0.075)
-    a = armature(0.012, 0.015, 0.07, 0.0000000175, 0, 6.384, 10, 10, 0.0505)
+    # a = armature(0.012, 0.015, 0.07, 0.0000000175, 0, 6.384, 70000, 3000, 0.0505)
+    a = armature(0.012, 0.015, 0.07, 0.0000000175, 0, 6.384, 1, 1, 0.0505)
     print("[OK]a initialized")
 
-    sscg = singleStageCoilgun(dc, a, 8200, 0.001, 0.001)
+    sscg = singleStageCoilgun(dc, a, 8000, 0.001, 0.001)
     print("[OK]sscg initialized")
 
-    (η, v) = sscg.run(1.2)
+    (η, v) = sscg.run(1)
     print("[RESULT]η = " + str(η * 100) + "%, RIGHT = 8.71%")
     print("[RESULT]v = " + str(v) + "m/s, RIGHT = 66.07m/s")
     print("[INFO]OVER")
@@ -122,6 +142,59 @@ def TestA():
     toc = time.perf_counter()
     t = toc - tic
     print("[DEBUG]TIME: " + str(t) + "s, " + str(t / 60) + "min.")
-    
+
+
+def TestB():
+    import time
+    tic = time.perf_counter()
+
+    print("[TEST]TEST B: READY----------------------------------------------------------------")
+
+    dc = drivingCoil(0.032, 0.05, 0.04, 40, 0.0000000175, 0.000028, 0.7)
+    print("[OK]dc initialized")
+
+    a = armature(0.02, 0.03, 10,  0.0000000175, 0, 0.6, 40, 10, 0.02)
+    print("[OK]a initialized")
+
+    sscg = singleStageCoilgun(dc, a, 4000, 0.001, 0.001)
+    print("[OK]sscg initialized")
+
+    (η, v) = sscg.run(1)
+    print("[RESULT]η = " + str(η * 100) + "%, RIGHT = 5.38%")
+    print("[RESULT]v = " + str(v) + "m/s, RIGHT = 38m/s")
+    print("[INFO]OVER")
+
+    toc = time.perf_counter()
+    t = toc - tic
+    print("[DEBUG]TIME: " + str(t) + "s, " + str(t / 60) + "min.")
+
+
+def TestC():
+    import time
+    tic = time.perf_counter()
+
+    print("[TEST]TEST C: READY----------------------------------------------------------------")
+
+    dc = drivingCoil(0.037, 0.05, 0.052, 66, 0.0000000175, 0.000028, 0.7)
+    print("[OK]dc initialized")
+
+    a = armature(0.023, 0.036, 0.05, 0.0000000175, 0, 0.6, 50, 13, 0.02)
+    print("[OK]a initialized")
+
+    sscg = singleStageCoilgun(dc, a, 4000, 0.001, 0.001)
+    print("[OK]sscg initialized")
+
+    (η, v) = sscg.run(1)
+    print("[RESULT]η = " + str(η * 100) + "%, RIGHT = 13.6%")
+    print("[RESULT]v = " + str(v) + "m/s, RIGHT = 61m/s")
+    print("[INFO]OVER")
+
+    toc = time.perf_counter()
+    t = toc - tic
+    print("[DEBUG]TIME: " + str(t) + "s, " + str(t / 60) + "min.")
+
+
 if __name__ == "__main__":
     TestA()
+    TestB()
+    TestC()
